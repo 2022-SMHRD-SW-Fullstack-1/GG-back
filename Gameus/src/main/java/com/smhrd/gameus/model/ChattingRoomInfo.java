406e1283451b75219279f32327d41deca4a77b38
ignaciocha <oesnahn@gmail.com>
2022-10-26 11:57:59 +0900

    Model 패키지 작성

--- Gameus/src/main/java/com/smhrd/gameus/model/ChattingRoomInfo.java
+++ Gameus/src/main/java/com/smhrd/gameus/model/ChattingRoomInfo.java
@@ -1,5 +1,25 @@
 package com.smhrd.gameus.model;
 
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
 public class ChattingRoomInfo {
 
+    // 방 순번 
+    private int cr_seq;
+
+    // 팀 순번 
+    private int team_seq;
+
+    // 방 제목 
+    private String cr_title;
+
+    // 방 내용 
+    private String cr_content;
+
+    // 방 개설일자 
+    private Timestamp cr_opendate;
+    
 }
