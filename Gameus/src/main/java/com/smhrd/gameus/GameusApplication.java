1c6aebe1897903c2761a570cddc584ddd2ba93e5
ignaciocha <oesnahn@gmail.com>
2022-10-26 10:33:16 +0900

    첫번째 커밋

---
+++ Gameus/src/main/java/com/smhrd/gameus/GameusApplication.java
@@ -0,0 +1,13 @@
+package com.smhrd.gameus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameusApplication.class, args);
+	}
+
+}
---
+++ Gameus/src/test/java/com/smhrd/gameus/GameusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.gameus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
