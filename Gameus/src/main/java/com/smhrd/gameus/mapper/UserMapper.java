8481aa6a154c55383e7aba74376eefc658ad5e14
zindol2 <yangjunho444@gmail.com>
2022-11-07 16:14:17 +0900

    로그인 기능

--- Gameus/src/main/java/com/smhrd/gameus/mapper/UserMapper.java
+++ Gameus/src/main/java/com/smhrd/gameus/mapper/UserMapper.java
@@ -1,9 +1,20 @@
 package com.smhrd.gameus.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.smhrd.gameus.model.UserInfo;
 
 @Mapper
 public interface UserMapper {
 
+
+
+	public void signupAdd(Map<String, Object> signupInfo);
 	
+	public String loginMember(UserInfo user);
 }
+
+
+
--- Gameus/src/main/java/com/smhrd/gameus/service/UserService.java
+++ Gameus/src/main/java/com/smhrd/gameus/service/UserService.java
@@ -1,8 +1,33 @@
 package com.smhrd.gameus.service;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.smhrd.gameus.mapper.UserMapper;
+import com.smhrd.gameus.model.UserInfo;
 
 @Service
 public class UserService {
 
-}
+
+		@Autowired
+		UserMapper userMapper;
+
+		public void signupAdd(Map<String, Object> signupInfo) {
+			userMapper.signupAdd(signupInfo);
+
+		}
+
+		
+		public String loginMember(UserInfo user) {
+			return userMapper.loginMember(user);
+	}
+		
+		
+		
+	}
+		
+	
+
